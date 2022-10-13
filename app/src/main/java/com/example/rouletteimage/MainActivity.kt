900f90477217381ec5d094e8e18d8408eb377fe7
HirayamaMasaki <masaki.hira1017@gmail.com>
2022-10-14 00:57:26 +0900

    1

--- app/src/main/java/com/example/rouletteimage/MainActivity.kt
+++ app/src/main/java/com/example/rouletteimage/MainActivity.kt
@@ -1,15 +1,30 @@
 package com.example.rouletteimage
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.*
+import android.graphics.Bitmap.CompressFormat
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.IOException
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val myView = MyView(this)
+//        setContentView(myView)
 
         val editTextTextPersonName = findViewById<EditText>(R.id.editTextTextPersonName)
         val editTextNumber1 = findViewById<EditText>(R.id.editTextNumber1)
@@ -31,4 +46,279 @@ class MainActivity : AppCompatActivity() {
             finish()
         }
     }
+
+//    override fun onStart() {
+//        super.onStart()
+////        setContentView(R.layout.activity_main)
+//
+//    }
+
+//     Viewを継承したクラス
+    internal inner class MyView(context: Context) : View(context) {
+
+        private var paint: Paint = TODO()
+        private val paint1 = Paint()
+        private val paint2 = Paint()
+        private val paint3 = Paint()
+        private val paint4 = Paint()
+        private val paint5 = Paint()
+        private val paint6 = Paint()
+        private val paint7 = Paint()
+        private val paint8 = Paint()
+        private val paint9 = Paint()
+        private val paint10 = Paint()
+        private val dm = DisplayMetrics()
+        internal val width = dm.widthPixels
+        internal val height = dm.heightPixels
+
+        private var sum by Delegates.notNull<Float>()
+
+        @SuppressLint("DrawAllocation")
+        override fun onDraw(canvas: Canvas){
+            rouletteMaking(canvas,1f,1f,2f,1f,1f,5f,4f,1f,1f,1f)
+        }
+
+        private fun rouletteMaking(canvas: Canvas, a:Float, b:Float?, c:Float?, d:Float?, e:Float?, f:Float?, g:Float?, h:Float?, i:Float?, j:Float?) {
+            // 背景、半透明
+            canvas.drawColor(Color.argb(0, 0, 0, 0))
+
+            // 円
+            paint.color = Color.argb(255, 0, 0, 0)
+            paint.strokeWidth = 5f
+            paint.isAntiAlias = true
+            paint.style = Paint.Style.STROKE
+
+            // 分岐
+            if (height >= width) {
+                // 円
+                // (x1,y1,r,paint) 中心x1座標, 中心y1座標, r半径
+                canvas.drawCircle((width / 2).toFloat(), (height / 2).toFloat(), (width / 2.5).toFloat(), paint)
+                // 各項目
+                val rect = RectF((width / 10).toFloat(), (height / 2 - 2 * width / 5).toFloat(), (9 * width / 10).toFloat(), (height / 2 + 2 * width / 5).toFloat())
+                itemRation(canvas,rect, a, b, c, d, e, f, g, h, i, j)
+
+            } else {
+                // 円
+                canvas.drawCircle((width / 2).toFloat(), (height / 2).toFloat(), (height / 2.5).toFloat(), paint)
+                //　各項目
+                val rect = RectF((width / 2 - 2 * height / 5).toFloat(), (height / 10).toFloat(), (width / 2 + 2 * height / 5).toFloat(), (9 * height / 10).toFloat())
+                itemRation(canvas,rect,a, b, c, d, e, f, g, h, i, j)
+            }
+
+            //保存用Bitmap準備
+            val image = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+            //新しいcanvasに保存用Bitmapをセット
+            Canvas(image)
+            //canvasに対して描画
+            try {
+                //出力ファイルを準備
+                val fos = FileOutputStream(File("sample.png"))
+                //PNG形式で出力
+                image.compress(CompressFormat.PNG, 100, fos)
+                fos.close()
+            } catch (e: FileNotFoundException) {
+                e.printStackTrace()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+        }
+
+        private fun itemRation(canvas: Canvas, rect: RectF, a:Float, b:Float?, c:Float?, d:Float?, e:Float?, f:Float?, g:Float?, h:Float?, i:Float?, j:Float?){
+            if (b != null && c != null && d != null && e != null && f != null && g != null && h != null && i != null && j != null) {
+                sum = a + b + c + d + e + f + g + h + i + j
+            } else if(b != null && c != null && d != null && e != null && f != null && g != null && h != null && i != null){
+                sum = a + b + c + d + e + f + g + h + i
+            } else if(b != null && c != null && d != null && e != null && f != null && g != null && h != null){
+                sum = a + b + c + d + e + f + g + h
+            } else if(b != null && c != null && d != null && e != null && f != null && g != null){
+                sum = a + b + c + d + e + f + g
+            } else if(b != null && c != null && d != null && e != null && f != null){
+                sum = a + b + c + d + e + f
+            } else if(b != null && c != null && d != null && e != null){
+                sum = a + b + c + d + e
+            } else if(b != null && c != null && d != null){
+                sum = a + b + c + d
+            } else if(b != null && c != null){
+                sum = a + b + c
+            } else if(b != null){
+                sum = a + b
+            } else{
+                sum = a
+            }
+
+            val a0 : Float = a/sum
+            val b0 : Float? = b?.div(sum)
+            val c0 : Float? = c?.div(sum)
+            val d0 : Float? = d?.div(sum)
+            val e0 : Float? = e?.div(sum)
+            val f0 : Float? = f?.div(sum)
+            val g0 : Float? = g?.div(sum)
+            val h0 : Float? = h?.div(sum)
+            val i0 : Float? = i?.div(sum)
+            val j0 : Float? = j?.div(sum)
+
+            paint1.color = Color.argb(195, 233, 58, 36)
+            canvas.drawArc(rect, -90F, a0*360, true, paint1)
+            paint2.color = Color.argb(195, 234, 97, 25)
+            if (b0 != null) {
+                canvas.drawArc(rect, -90F+a0*360, b0*360, true, paint2)
+            }
+            paint3.color = Color.argb(195, 252, 202, 0)
+            if (c0 != null) {
+                canvas.drawArc(rect, -90F+(a0+ b0!!)*360, c0*360, true, paint3)
+            }
+            paint4.color = Color.argb(195, 184, 198, 1)
+            if (d0 != null) {
+                canvas.drawArc(rect, -90F+(a0+ b0!!+ c0!!)*360, d0*360, true, paint4)
+            }
+            paint5.color = Color.argb(195, 58, 149, 42)
+            if (e0 != null) {
+                canvas.drawArc(rect, -90F+(a0+ b0!!+ c0!!+ d0!!)*360, e0*360, true, paint5)
+            }
+            paint6.color = Color.argb(195, 10, 151, 114)
+            if (f0 != null) {
+                canvas.drawArc(rect, -90F+(a0+ b0!!+ c0!!+ d0!!+ e0!!)*360, f0*360, true, paint6)
+            }
+            paint7.color = Color.argb(195, 24, 158, 151)
+            if (g0 != null) {
+                canvas.drawArc(rect, -90F+(a0+ b0!!+ c0!!+ d0!!+ e0!!+ f0!!)*360, g0*360, true, paint7)
+            }
+            paint8.color = Color.argb(195, 89, 113, 157)
+            if (h0 != null) {
+                canvas.drawArc(rect, -90F+(a0+ b0!!+ c0!!+ d0!!+ e0!!+ f0!!+ g0!!)*360, h0*360, true, paint8)
+            }
+            paint9.color = Color.argb(195, 104, 68, 126)
+            if (i0 != null) {
+                canvas.drawArc(rect, -90F+(a0+ b0!!+ c0!!+ d0!!+ e0!!+ f0!!+ g0!!+ h0!!)*360, i0*360, true, paint9)
+            }
+            paint10.color = Color.argb(195, 224, 61, 114)
+            if (j0 != null) {
+                canvas.drawArc(rect, -90F+(a0+ b0!!+ c0!!+ d0!!+ e0!!+ f0!!+ g0!!+ h0!!+ i0!!)*360, j0*360, true, paint10)
+            }
+        }
+    }
+
+
+//    @RequiresApi(Build.VERSION_CODES.N)
+//    private fun rouletteMaking(canvas: Canvas, a:Float, b:Float?, c:Float?, d:Float?, e:Float?, f:Float?, g:Float?, h:Float?, i:Float?, j:Float?) {
+//        // 背景、半透明
+//        canvas.drawColor(Color.argb(0, 0, 0, 0))
+//
+//        // 円
+//        paint.color = Color.argb(255, 0, 0, 0)
+//        paint.strokeWidth = 5f
+//        paint.isAntiAlias = true
+//        paint.style = Paint.Style.STROKE
+//
+//        // 分岐
+//        if (height >= width) {
+//            // 円
+//            // (x1,y1,r,paint) 中心x1座標, 中心y1座標, r半径
+//            canvas.drawCircle((width / 2).toFloat(), (height / 2).toFloat(), (width / 2.5).toFloat(), paint)
+//            // 各項目
+//            val rect = RectF((width / 10).toFloat(), (height / 2 - 2 * width / 5).toFloat(), (9 * width / 10).toFloat(), (height / 2 + 2 * width / 5).toFloat())
+//            itemRation(canvas,rect, a, b, c, d, e, f, g, h, i, j)
+//
+//        } else {
+//            // 円
+//            canvas.drawCircle((width / 2).toFloat(), (height / 2).toFloat(), (height / 2.5).toFloat(), paint)
+//            //　各項目
+//            val rect = RectF((width / 2 - 2 * height / 5).toFloat(), (height / 10).toFloat(), (width / 2 + 2 * height / 5).toFloat(), (9 * height / 10).toFloat())
+//            itemRation(canvas,rect,a, b, c, d, e, f, g, h, i, j)
+//        }
+//
+//        //保存用Bitmap準備
+//        val image = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+//        //新しいcanvasに保存用Bitmapをセット
+//        Canvas(image)
+//        //canvasに対して描画
+//        try {
+//            //出力ファイルを準備
+//            val fos = FileOutputStream(File("sample.png"))
+//            //PNG形式で出力
+//            image.compress(CompressFormat.PNG, 100, fos)
+//            fos.close()
+//        } catch (e: FileNotFoundException) {
+//            e.printStackTrace()
+//        } catch (e: IOException) {
+//            e.printStackTrace()
+//        }
+//
+//    }
+//
+//    private fun itemRation(canvas: Canvas, rect: RectF, a:Float, b:Float?, c:Float?, d:Float?, e:Float?, f:Float?, g:Float?, h:Float?, i:Float?, j:Float?){
+//        if (b != null && c != null && d != null && e != null && f != null && g != null && h != null && i != null && j != null) {
+//            sum = a + b + c + d + e + f + g + h + i + j
+//        } else if(b != null && c != null && d != null && e != null && f != null && g != null && h != null && i != null){
+//            sum = a + b + c + d + e + f + g + h + i
+//        } else if(b != null && c != null && d != null && e != null && f != null && g != null && h != null){
+//            sum = a + b + c + d + e + f + g + h
+//        } else if(b != null && c != null && d != null && e != null && f != null && g != null){
+//            sum = a + b + c + d + e + f + g
+//        } else if(b != null && c != null && d != null && e != null && f != null){
+//            sum = a + b + c + d + e + f
+//        } else if(b != null && c != null && d != null && e != null){
+//            sum = a + b + c + d + e
+//        } else if(b != null && c != null && d != null){
+//            sum = a + b + c + d
+//        } else if(b != null && c != null){
+//            sum = a + b + c
+//        } else if(b != null){
+//            sum = a + b
+//        } else{
+//            sum = a
+//        }
+//
+//        val a0 : Float = a/sum
+//        val b0 : Float? = b?.div(sum)
+//        val c0 : Float? = c?.div(sum)
+//        val d0 : Float? = d?.div(sum)
+//        val e0 : Float? = e?.div(sum)
+//        val f0 : Float? = f?.div(sum)
+//        val g0 : Float? = g?.div(sum)
+//        val h0 : Float? = h?.div(sum)
+//        val i0 : Float? = i?.div(sum)
+//        val j0 : Float? = j?.div(sum)
+//
+//        paint1.color = Color.argb(195, 233, 58, 36)
+//        canvas.drawArc(rect, -90F, a0*360, true, paint1)
+//        paint2.color = Color.argb(195, 234, 97, 25)
+//        if (b0 != null) {
+//            canvas.drawArc(rect, -90F+a0*360, b0*360, true, paint2)
+//        }
+//        paint3.color = Color.argb(195, 252, 202, 0)
+//        if (c0 != null) {
+//            canvas.drawArc(rect, -90F+(a0+ b0!!)*360, c0*360, true, paint3)
+//        }
+//        paint4.color = Color.argb(195, 184, 198, 1)
+//        if (d0 != null) {
+//            canvas.drawArc(rect, -90F+(a0+ b0!!+ c0!!)*360, d0*360, true, paint4)
+//        }
+//        paint5.color = Color.argb(195, 58, 149, 42)
+//        if (e0 != null) {
+//            canvas.drawArc(rect, -90F+(a0+ b0!!+ c0!!+ d0!!)*360, e0*360, true, paint5)
+//        }
+//        paint6.color = Color.argb(195, 10, 151, 114)
+//        if (f0 != null) {
+//            canvas.drawArc(rect, -90F+(a0+ b0!!+ c0!!+ d0!!+ e0!!)*360, f0*360, true, paint6)
+//        }
+//        paint7.color = Color.argb(195, 24, 158, 151)
+//        if (g0 != null) {
+//            canvas.drawArc(rect, -90F+(a0+ b0!!+ c0!!+ d0!!+ e0!!+ f0!!)*360, g0*360, true, paint7)
+//        }
+//        paint8.color = Color.argb(195, 89, 113, 157)
+//        if (h0 != null) {
+//            canvas.drawArc(rect, -90F+(a0+ b0!!+ c0!!+ d0!!+ e0!!+ f0!!+ g0!!)*360, h0*360, true, paint8)
+//        }
+//        paint9.color = Color.argb(195, 104, 68, 126)
+//        if (i0 != null) {
+//            canvas.drawArc(rect, -90F+(a0+ b0!!+ c0!!+ d0!!+ e0!!+ f0!!+ g0!!+ h0!!)*360, i0*360, true, paint9)
+//        }
+//        paint10.color = Color.argb(195, 224, 61, 114)
+//        if (j0 != null) {
+//            canvas.drawArc(rect, -90F+(a0+ b0!!+ c0!!+ d0!!+ e0!!+ f0!!+ g0!!+ h0!!+ i0!!)*360, j0*360, true, paint10)
+//        }
+//    }
+
 }
--- app/src/main/java/com/example/rouletteimage/RouletteScreen.kt
+++ app/src/main/java/com/example/rouletteimage/RouletteScreen.kt
@@ -1,25 +1,21 @@
 package com.example.rouletteimage
 
-import android.content.Context
+import android.annotation.SuppressLint
 import android.content.Intent
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
 import android.os.Bundle
-import android.util.DisplayMetrics
-import android.view.View
+import android.os.Handler
+import android.view.animation.AnimationSet
+import android.view.animation.DecelerateInterpolator
+import android.view.animation.RotateAnimation
 import android.widget.Button
-import android.widget.FrameLayout
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+
 class RouletteScreen : AppCompatActivity() {
 
-    lateinit var paint: Paint
-    val dm = DisplayMetrics()
-    val width = dm.widthPixels
-    val height = dm.heightPixels
-
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_roulette_screen)
@@ -28,68 +24,76 @@ class RouletteScreen : AppCompatActivity() {
         val button3 = findViewById<Button>(R.id.button3)
         val button4 = findViewById<Button>(R.id.button4)
         val text3 = findViewById<TextView>(R.id.textView3)
-        val fl = findViewById<FrameLayout>(R.id.fl)
+//        val animation1 = ResourcesCompat.getDrawable(
+//            resources, R.drawable.animation1,null
+//        )
+        val image2 = findViewById<ImageView>(R.id.imageView2)
+//        val fl = findViewById<FrameLayout>(R.id.fl)
 
         val etText1 = intent.getStringExtra("key1")
         val etText2 = intent.getStringExtra("key2")
         val etText3 = intent.getStringExtra("key3")
 
+//        通常回転系
+        val animSet0 = AnimationSet(true)
+        animSet0.interpolator = DecelerateInterpolator()
+        animSet0.fillAfter = true
+        animSet0.isFillEnabled = true
+        val animRotate0 = RotateAnimation(
+            0.0f, -600000.0f,
+            RotateAnimation.RELATIVE_TO_SELF, 0.5f,
+            RotateAnimation.RELATIVE_TO_SELF, 0.5f
+        )
+        animRotate0.duration = 250000
+        animRotate0.fillAfter = true
+        animSet0.addAnimation(animRotate0)
+
+//        停止回転系
+        val animSet1 = AnimationSet(true)
+        animSet1.interpolator = DecelerateInterpolator()
+        animSet1.fillAfter = true
+        animSet1.isFillEnabled = true
+        val animRotate1 = RotateAnimation(
+            0.0f, -3600.0f,
+            RotateAnimation.RELATIVE_TO_SELF, 0.5f,
+            RotateAnimation.RELATIVE_TO_SELF, 0.5f
+        )
+        animRotate1.duration = 2500
+        animRotate1.fillAfter = true
+        animSet1.addAnimation(animRotate1)
+
         text3.text = etText1
         button3.isEnabled = false
 
+//        回すボタン
         button2.setOnClickListener {
+            image2.startAnimation(animSet0)
+
             button2.isEnabled = false
             button3.isEnabled = true
+
+            Handler().postDelayed( {
+                if(button3.isEnabled){
+                    button3.performClick()
+                }
+                                   }, 10000)
         }
 
+//        止めるボタン
         button3.setOnClickListener {
+            image2.startAnimation(animSet1)
+
             button2.isEnabled = true
             button3.isEnabled = false
         }
 
+//        戻るボタン
         button4.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
         }
 
-        val myView = MyView(this)
-        setContentView(myView)
-
     }
-
-    // Viewを継承したクラス
-    class MyView(context: Context) : View(context) {
-        private var paint: Paint = Paint()
-
-        override fun onDraw(canvas: Canvas){
-            // 背景、半透明
-            canvas.drawColor(Color.argb(255, 255, 255, 255))
-
-            // 円
-            paint.setColor(Color.argb(255, 0, 0, 0))
-            paint.setStrokeWidth(3F)
-            paint.setAntiAlias(true)
-            paint.setStyle(Paint.Style.STROKE)
-            // (x1,y1,r,paint) 中心x1座標, 中心y1座標, r半径
-            canvas.drawCircle((width/2).toFloat(), (height/2).toFloat(), (width/2.5).toFloat(), paint)
-
-        }
-    }
-
-
-//    fun onDraw(canvas: Canvas) {
-//        // 背景、半透明
-//        canvas.drawColor(Color.argb(255, 255, 255, 255))
-//
-//        // 円
-//        paint.setColor(Color.argb(255, 0, 0, 0))
-//        paint.setStrokeWidth(3F)
-//        paint.setAntiAlias(true)
-//        paint.setStyle(Paint.Style.STROKE)
-//        // (x1,y1,r,paint) 中心x1座標, 中心y1座標, r半径
-//        canvas.drawCircle((width/2).toFloat(), (height/2).toFloat(), (width/2.5).toFloat(), paint)
-//
-//    }
 }
 
